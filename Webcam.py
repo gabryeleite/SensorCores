955b84c596a18a8147c51315f65d015bc268b0c0
Gabryel Leite <96426826+gabryeleite@users.noreply.github.com>
2024-01-31 08:48:19 -0300

    Update Webcam.py

--- Webcam.py
+++ Webcam.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from colors import Color, cores
+from BancoCores import Color, cores
 
 def get_color(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -17,25 +17,24 @@ def get_color(event, x, y, flags, param):
 
         print("Cor: {} ({}, {}, {})".format(cores[melhorCorrespondencia].nome, r, g, b))
 
-# Inicializa a captura de vídeo da webcam
+# inicializa a captura de vídeo da webcam
 cap = cv2.VideoCapture(0)
 
-# Define a função de callback para o evento de clique do mouse
+# define a função de callback para o evento de clique do mouse
 cv2.namedWindow("Webcam")
 cv2.setMouseCallback("Webcam", get_color)
 
-# Loop infinito
 while True:
-    # Captura o próximo frame de vídeo
+    # captura o próximo frame de vídeo
     ret, frame = cap.read()
 
-    # Exibe o frame de vídeo na janela
+    # exibe o frame de vídeo na janela
     cv2.imshow("Webcam", frame)
 
-    # Sai do loop se a tecla 'q' for pressionada
+    # sai do loop se a tecla 'q' for pressionada
     if cv2.waitKey(1) & 0xFF == ord('s'):
         break
 
-# Libera os recursos
+# libera os recursos
 cap.release()
 cv2.destroyAllWindows()
